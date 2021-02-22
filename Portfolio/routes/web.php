915ad07a1dafb541c51fa372a0323793aa88fe24
ssultann <190103449@stu.sdu.edu.kz>
2021-02-22 23:02:56 +0500

    adding html

--- Portfolio/routes/web.php
+++ Portfolio/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
